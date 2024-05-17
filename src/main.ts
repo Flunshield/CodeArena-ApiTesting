3b5df1ddfcfde083adf20cff715408bffa94a42d
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-05-17 12:39:07 +0200

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true, // ou true pour permettre à tous les domaines
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true, // autoriser les cookies et les en-têtes d'autorisation
-  });
   await app.listen(3001);
 }
 bootstrap();
